ece3e3cdc014ceb838fa9c1c6d0a3b216178ba65
Maksym Hontar <maxim.gontar@gmail.com>
2017-12-10 20:05:45 +0200

    Added security fixes from html injection

--- php-test.loc/data-insert.php
+++ php-test.loc/data-insert.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'db-config.php';
 
-$messageNick = $_POST['nick']==''?"anonymous":$_POST['nick'];
-$messageText = $_POST['message'];
+$messageNick = htmlspecialchars($_POST['nick']==''?"anonymous":$_POST['nick']);
+$messageText = htmlspecialchars($_POST['message']);
 $messageTime = time();
 
 $query = "INSERT INTO `messages`(`nick`, `message`, `time`) VALUES ('".$messageNick."', '".$messageText."', '".$messageTime."')";
